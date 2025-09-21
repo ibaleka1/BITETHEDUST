236f632c504984a03524bbb42306ce5f352924af
ibaleka1 <elvec2kw@gmail.com>
2025-09-21 11:15:41 -0400

    Add ExercisesSection component with exercises

--- src/components/ExercisesSection.tsx
+++ src/components/ExercisesSection.tsx
@@ -1,1 +1,13 @@
+import BreathingOrb from "./BreathingOrb";
+import GroundingExercise from "./GroundingExercise";
+import SwayingOrbit from "./SwayingOrbit";
 
+export default function ExercisesSection() {
+  return (
+    <section id="experience">
+      <BreathingOrb />
+      <GroundingExercise />
+      <SwayingOrbit />
+    </section>
+  );
+}
