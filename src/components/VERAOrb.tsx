4a522f31966b23fdca78586464c84c8bb6319c9b
ibaleka1 <elvec2kw@gmail.com>
2025-09-21 09:33:28 -0400

    Refactor VERAOrb component structure

--- src/components/VERAOrb.tsx
+++ src/components/VERAOrb.tsx
@@ -21,11 +21,20 @@ const VERAOrb: React.FC = () => {
 
   return (
     <div className="vera-orb-wrapper">
-      <div ref={orbRef} className="vera-orb" onClick={activate} tabIndex={0} role="button" aria-label="Activate VERA">
+      <div
+        ref={orbRef}
+        className="vera-orb"
+        onClick={activate}
+        tabIndex={0}
+        role="button"
+        aria-label="Activate VERA"
+      >
         <span className="vera-orb-glow"></span>
         <span className="vera-orb-inner"></span>
       </div>
-      <div style={{marginTop: 12, color: "#555"}}>Click the orb to activate VERA</div>
+      <div style={{ marginTop: 12, color: "#555" }}>
+        Click the orb to activate VERA
+      </div>
     </div>
   );
 };
