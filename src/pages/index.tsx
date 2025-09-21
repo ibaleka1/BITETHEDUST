0238b134462806c11401c37ef6f2720571789daa
ibaleka1 <elvec2kw@gmail.com>
2025-09-21 09:31:20 -0400

    Add VERAOrb component to Home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,1 +1,11 @@
+import React from "react";
+import VERAOrb from "../components/VERAOrb";
 
+export default function Home() {
+  return (
+    <main style={{minHeight:"100vh",background:"#e0f7fa",padding:0}}>
+      <h1 style={{textAlign:"center",marginTop:"40px",color:"#00796b"}}>Welcome to VERA</h1>
+      <VERAOrb />
+    </main>
+  );
+}
