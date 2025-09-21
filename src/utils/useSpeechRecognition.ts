1418715a96ada851900ecfd4465a6dcf2c99d2d1
ibaleka1 <elvec2kw@gmail.com>
2025-09-21 09:45:44 -0400

    Add speak function for text-to-speech

--- src/utils/useSpeechRecognition.ts
+++ src/utils/useSpeechRecognition.ts
@@ -1,1 +1,7 @@
-
+export function speak(text: string) {
+  if ("speechSynthesis" in window) {
+    const utterance = new window.SpeechSynthesisUtterance(text);
+    utterance.lang = "en-US";
+    window.speechSynthesis.speak(utterance);
+  }
+}
