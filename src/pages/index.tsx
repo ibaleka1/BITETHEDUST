1fbf84bb991da9aa04fb2e3a5373f916332fd8d2
ibaleka1 <elvec2kw@gmail.com>
2025-09-21 09:32:54 -0400

    Fix formatting of welcome message in index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,8 +3,10 @@ import VERAOrb from "../components/VERAOrb";
 
 export default function Home() {
   return (
-    <main style={{minHeight:"100vh",background:"#e0f7fa",padding:0}}>
-      <h1 style={{textAlign:"center",marginTop:"40px",color:"#00796b"}}>Welcome to VERA</h1>
+    <main style={{ minHeight: "100vh", background: "#e0f7fa", padding: 0 }}>
+      <h1 style={{ textAlign: "center", marginTop: "40px", color: "#00796b" }}>
+        Welcome to VERA
+      </h1>
       <VERAOrb />
     </main>
   );
