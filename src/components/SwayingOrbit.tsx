d3f7675b53d649ea4656d319bf9939e96d21733c
ibaleka1 <elvec2kw@gmail.com>
2025-09-21 10:09:57 -0400

    Add SwayingOrbit component with styling and text

---
+++ src/components/SwayingOrbit.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./SwayingOrbit.css";
+
+export default function SwayingOrbit() {
+  return (
+    <div className="swaying-orbit-container">
+      <div className="swaying-orbit">
+        <div className="swaying-orbit-inner"></div>
+      </div>
+      <div className="swaying-orbit-text">
+        Sway with the rhythm. Calm your mind.
+      </div>
+    </div>
+  );
+}
